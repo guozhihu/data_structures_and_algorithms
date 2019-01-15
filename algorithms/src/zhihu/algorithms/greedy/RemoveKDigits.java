3d0571d7a3c6c20a3001e56fa4b30fe30ac3175d
guozhihao <283839751@qq.com>
2019-01-15 19:01:10 +0800

    贪心算法-移除K个数，LeetCode第402题Remove K Digits

---
+++ algorithms/src/zhihu/algorithms/greedy/RemoveKDigits.java
@@ -0,0 +1,85 @@
+package zhihu.algorithms.greedy;
+
+import java.util.Stack;
+
+/**
+ * Author: zhihu
+ * Description: 移除K个数字
+ * 给定一个非负整数num表示为字符串，从num中删除k个数字，求获得最小的可能的新数字。
+ * 注意：
+ * num的长度小于10002并且≥k。
+ * 给定的num不包含任何前导零。
+ * 例1：
+ * <p>
+ * 输入：num =“1432219”，k = 3
+ * 输出：“1219”
+ * 说明：删除三个数字4,3和2以形成最小的新数字1219。
+ * 例2：
+ * <p>
+ * 输入：num =“10200”，k = 1
+ * 输出：“200”
+ * 说明：删除前导1，数字为200.请注意，输出不得包含前导零。
+ * 例3：
+ * <p>
+ * 输入：num =“10”，k = 2
+ * 输出：“0”
+ * 说明：从数字中删除所有数字，并且没有任何值为0。
+ * Date: Create in 2019/1/15 15:09
+ * 难度：Medium
+ */
+public class RemoveKDigits {
+    
+    /**
+     * 算法思路：
+     * 取出字符串的每一个字符转化为数字
+     * 将取出的数字入栈，入栈前，如果栈元素个数大于0并且k也大于0，不断迭代栈顶元素，如果将要入栈的数字小于栈顶，则将栈顶元素踢出栈，每一次栈顶出栈，k自减1
+     * 数字入栈
+     * 将所有数字入栈后，如果k>0,则从栈顶开始踢出数字，直至踢出k个
+     * 最后从栈底开始遍历，拼装字符串，踢出前导零后开始拼装数字
+     * @param num
+     * @param k
+     * @return
+     */
+    public static String removeKdigits(String num, int k) {
+        // 如果字符串长度==移除的个数k，则直接返回“0”字符串
+        if (num.length() == k) {
+            return "0";
+        }
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < num.length(); i++) {
+            int number = num.charAt(i) - '0';
+            // 入栈前迭代栈顶元素，判断栈顶元素是否大于当前的字符数值，如果是，剔除该栈顶元素
+            while (stack.size() > 0 && k > 0 && number < stack.peek()) {
+                stack.pop();
+                k--;
+            }
+            stack.push(number);
+        }
+        // num字符串的每个字符都入栈后，k仍然大于0，则从栈顶开始踢出元素，直至k为0
+        while (k-- > 0 && stack.size() > 0) {
+            stack.pop();
+        }
+        StringBuilder sb = new StringBuilder();
+        
+        // 剔除前导零
+        boolean LeadingZeroFlag = true; // 前导0是否结束标志
+        for (int i = 0; i < stack.size(); i++) {
+            int numString = stack.get(i);
+            // 判断该数字是不是符合前导零
+            if (numString != 0) {
+                LeadingZeroFlag = false;
+            }
+            if (!LeadingZeroFlag) {
+                sb.append(numString);
+            }
+        }
+        return sb.toString().length() > 0 ? sb.toString() : "0";
+    }
+    
+    public static void main(String[] args) {
+        String num = "1432219";
+        int k = 3;
+        System.out.println(removeKdigits(num, k));
+    }
+}
