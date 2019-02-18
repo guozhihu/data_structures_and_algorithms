ee2ae744cfab0ea818b8677b9fade21484f4c53c
guozhihao <283839751@qq.com>
2019-02-18 16:04:35 +0800

    排序算法-插入排序

---
+++ algorithms/src/zhihu/algorithms/sort/insertionSort.java
@@ -0,0 +1,30 @@
+package zhihu.algorithms.sort;
+
+/**
+ * Author: zhihu
+ * Description: 插入排序
+ * Date: Create in 2019/2/18 16:02
+ */
+public class insertionSort {
+    
+    public static void insertionSort(int[] arr) {
+        /**
+         * 算法思想：假设前面的序列是有序序列，现在将后面的元素插入到该有序序列中
+         */
+        for (int i = 1; i < arr.length; i++) {
+            // 取出第i个位置的值并与前面的有序序列的各个元素进行比较，将其插入适当的位置
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    
+    private static void swap(int[] arr, int j, int i) {
+//		int tmp = arr[i];
+//		arr[i] = arr[j];
+//		arr[j] = tmp;
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+}
