b22ef7f3e5c11ec4970cc783cc21de36089214d2
guozhihao <283839751@qq.com>
2019-02-25 11:43:19 +0800

    基于动态数组实现的栈

---
+++ data_structures/src/zhihu/datastructures/stack/ArrayStack.java
@@ -0,0 +1,64 @@
+package zhihu.datastructures.stack;
+
+import zhihu.datastructures.array.Array;
+
+/**
+ * Author: zhihu
+ * Description: 基于动态数组实现的栈
+ * Date: Create in 2019/2/25 11:38
+ */
+public class ArrayStack<E> implements Stack<E> {
+    
+    private Array<E> array;
+    
+    public ArrayStack(int capacity) {
+        array = new Array<>(capacity);
+    }
+    
+    public ArrayStack() {
+        array = new Array<>();
+    }
+    
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+    
+    public int getCapacity() {
+        return array.getCapacity();
+    }
+    
+    @Override
+    public void push(E e) {
+        array.addLast(e);
+    }
+    
+    @Override
+    public E pop() {
+        return array.removeLast();
+    }
+    
+    @Override
+    public E peek() {
+        return array.getLast();
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Stack: ");
+        res.append('[');
+        for (int i = 0; i < array.getSize(); i++) {
+            res.append(array.get(i));
+            if (i != array.getSize() - 1)
+                res.append(", ");
+        }
+        res.append("] top");
+        return res.toString();
+    }
+}
