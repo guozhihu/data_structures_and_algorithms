8288775acefa35ba34d13060a5cca2152caaebb6
guozhihu <283839751@qq.com>
2019-04-08 16:48:22 +0800

    堆的应用-随时找到数据流的中位数

--- data_structures/src/zhihu/datastructures/heap/MaxHeap.java
+++ data_structures/src/zhihu/datastructures/heap/MaxHeap.java
@@ -18,8 +18,11 @@ public class MaxHeap<E extends Comparable<E>> {
         data = new Array<>();
     }
     // 这里采用heapify将一个数组转换成最大堆，算法复杂度为O(n),如果采用将数组中的元素依次添加到一个空堆中，其算法复杂度为O(nlogn)
+    // 如果采用向空堆中插入元素，则对于每个元素都要执行一次上升操作
+    // 而对于heapify则可以不用管叶子节点，直接从非叶子节点开始执行一次下降操作即可
     public MaxHeap(E[] arr) {
         data = new Array<>(arr);
+        // 从第一个非叶子节点开始进行下降操作
         for (int i = parent(arr.length - 1); i >= 0; i--)
             siftDown(i);
     }
@@ -84,6 +87,7 @@ public class MaxHeap<E extends Comparable<E>> {
         return ret;
     }
     
+    // 下降操作，时间复杂度为O(logn)
     private void siftDown(int k) {
         
         while (leftChild(k) < data.getSize()) {
@@ -102,6 +106,9 @@ public class MaxHeap<E extends Comparable<E>> {
     }
     
     // 取出堆中的最大元素，并且替换成元素e
+    // 这里有两种实现方式：
+    // 第一种：可以先extractMax，再add，两次O(logn)的操作
+    // 第二种：可以直接将堆顶元素替换以后siftDown，一次O(logn)的操作
     public E replace(E e) {
         
         E ret = findMax();
