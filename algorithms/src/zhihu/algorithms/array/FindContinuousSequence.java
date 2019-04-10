cf4b85ce47e2ca375587265f13af066108549ab7
guozhihu <283839751@qq.com>
2019-04-10 17:41:31 +0800

    数组-和为S的连续正数序列

--- algorithms/src/zhihu/algorithms/array/FindContinuousSequence.java
+++ algorithms/src/zhihu/algorithms/array/FindContinuousSequence.java
@@ -4,7 +4,14 @@ import java.util.ArrayList;
 
 /**
  * Author: zhihu
- * Description:
+ * Description: 和为S的连续正数序列
+ * 小明很喜欢数学,有一天他在做数学作业时,要求计算出9~16的和,他马上就写出了正确答案是100。
+ * 但是他并不满足于此,他在想究竟有多少种连续的正数序列的和为100(至少包括两个数)。没多久,
+ * 他就得到另一组连续正数和为100的序列:18,19,20,21,22。现在把问题交给你,你能不能也很快
+ * 的找出所有和为S的连续正数序列? Good Luck!
+ * <p>
+ * 输出描述:
+ * 输出所有和为S的连续正数序列。序列内按照从小至大的顺序，序列间按照开始数字从小到大的顺序
  * Date: Create in 2019/4/10 16:55
  */
 public class FindContinuousSequence {
@@ -19,6 +26,7 @@ public class FindContinuousSequence {
      * 若a1到an序列和小于S,则an++;
      * 若a1到an序列和大于于S，则将a1++。
      * 当a1等于(S+1)/2时停止。
+     *
      * @param sum
      * @return
      */
@@ -54,5 +62,15 @@ public class FindContinuousSequence {
             }
         }
         return result;
+    }
+    
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> result = findContinuousSequence(100);
+        for (ArrayList<Integer> sequenceList : result) {
+            for (Integer sequence : sequenceList) {
+                System.out.print(sequence + "\t");
+            }
+            System.out.println();
+        }
     }
 }
